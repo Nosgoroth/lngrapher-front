d0cbdd72bafa5e62d15f22b247544448852587e3
Nosgoroth <Nosgoroth@Skylab-2.local>
2022-10-10 10:33:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lngrapher',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
